2e7aa7907f6d7d2d7cdacf9ceb9a9f8fe47084f5
K!a <kialakunngalgal@gmail.com>
2024-10-09 15:10:51 +1000

    Create livecd_tools_setup.sh

---
+++ livecd_tools_setup.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo "Installing patch and livecd-tools..."
+
+dnf install patch livecd-tools
+
+echo "Downloading livecd-iso-to-disk patch..."
+
+curl -L -o livecd-iso-to-disk.patch.txt https://github.com/livecd-tools/livecd-tools/files/11346168/livecd-iso-to-disk.f38.patch.txt
+
+echo "Applying patch..."
+
+patch -i livecd-iso-to-disk.patch.txt -d /usr/bin
+
+echo "Completed."
+
+echo "Create live OS with persistance overlay by running the following in your terminal: "
+echo "# sudo livecd-iso-to-disk --overlay-size-mb 10000 --home-size-mb 10000 --unencrypted-home --nomac --efi --format Fedora.iso /dev/sdX" 
