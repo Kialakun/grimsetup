56737c4e3ba6eee1c85681591c5f7ed5c4b3cd23
K!a <kialakunngalgal@gmail.com>
2024-10-11 10:18:24 +1000

    Create install_code.sh

---
+++ install_code.sh
@@ -0,0 +1,10 @@
+echo "Setting up..."
+
+rpm --import https://packages.microsoft.com/keys/microsoft.asc
+echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" | sudo tee /etc/yum.repos.d/vscode.repo > /dev/null
+
+echo "Installing code..."
+
+dnf isntall code
+
+echo "Complete."
