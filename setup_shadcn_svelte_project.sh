ac044463cd220cd076674abbe68d3e2b71b61466
K!a <kialakunngalgal@gmail.com>
2024-10-30 19:15:10 +1000

    Update and rename setup_shadcn_svelte_project to setup_shadcn_svelte_project.sh

---
+++ setup_shadcn_svelte_project.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+echo "Creating SveltKit app named $1"
+
+npx sv create $1
+
+npx sv add tailwindcss
+
+npm install
+
+echo "
+Setup path aliases
+
+If you are not using the default alias $lib, you'll need to update your svelte.config.js file to include those aliases.
+svelte.config.js
+
+	
+const config = {
+  // ... other config
+  kit: {
+    // ... other config
+    alias: {
+      "@/*": "./path/to/lib/*",
+    },
+  },
+};
+
+# Then run the cli 
+
+npx  shadcn-svelte@next init
+"
