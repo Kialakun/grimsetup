aee67d9ca147f2a421b044d7225f49fb0701ddba
K!a <kialakunngalgal@gmail.com>
2025-07-24 15:13:36 +1000

    Update install_edge.sh

--- install_edge.sh
+++ install_edge.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 
 sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
-sudo dnf config-manager addrepo --from-repofile https://packages.microsoft.com/yumrepos/config.repo
+sudo dnf config-manager addrepo --from-repofile https://packages.microsoft.com/yumrepos/edge/config.repo
 sudo dnf install microsoft-edge-stable
